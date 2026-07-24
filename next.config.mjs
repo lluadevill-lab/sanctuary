dbd86e3a8e89d0e2165e0992e35ea78617c059a3
lluadevill-lab <lluadevill@gmail.com>
2026-07-24 16:20:00 -0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**.supabase.co" },
+      { protocol: "https", hostname: "diablo2.com.br" },
     ],
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
